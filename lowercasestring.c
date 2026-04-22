4edbf2e5eaa7e4a584d406db8ce5ece93d447543
Juliet Jomon <julietjomon2021@gmail.com>
2026-04-22 12:44:46 +0530

    Create lowercasestring.c

---
+++ lowercasestring.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter string: ");
+    scanf("%s",str);
+
+    for(i=0; str[i]!='\0'; i++)
+        str[i]=tolower(str[i]);
+
+    printf("Result: %s",str);
+    return 0;
+}
+
+output
+Enter string: WELCOME
+Result: welcome
+
+=== Code Execution Successful ===

f4c228c9865d98e4a34b5476213be338131ca29a
Juliet Jomon <julietjomon2021@gmail.com>
2026-04-22 12:46:17 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num,sum;
+
+    printf("Enter number: ");
+    scanf("%d",&num);
+
+    while(num>9){
+        sum=0;
+        while(num>0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        num=sum;
+    }
+
+    printf("Result: %d",num);
+    return 0;
+}
+
+output
+Enter number: 987
+Result: 6
+
+=== Code Execution Successful ===

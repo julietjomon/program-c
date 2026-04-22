ace1aa7961516789f86d3c6f5e4f6cb41b448b22
Juliet Jomon <julietjomon2021@gmail.com>
2026-04-22 12:50:06 +0530

    Create addtwonum.c

---
+++ addtwonum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a,b,sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+
+    sum=a+b;
+
+    printf("Sum = %d",sum);
+    return 0;
+}
+
+output
+Enter two numbers: 50
+50
+Sum = 100
+
+=== Code Execution Successful ===
